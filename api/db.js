c23710f8397c75bfedc2437b68a57e48073aa8b1
unknown <jenna@acubetechnologies.in>
2025-03-10 15:30:29 +0530

    Initial commit

---
+++ api/db.js
@@ -0,0 +1,22 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  port: process.env.MYSQL_ADDON_PORT
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("❌ Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("✅ Connected to database.");
+});
+
+export { db };
